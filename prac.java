77b8bdf5f809432c6af9850a8719f9759ab4e4aa
Ashish jha <ajha5645@gmail.com>
2025-01-09 00:11:04 +0530

    new data

---
+++ prac.java
@@ -0,0 +1,12 @@
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class prac {
+   PriorityQueue<Integer>= new PriorityQueue<>();
+   Comparator
+    @Retention(RetentionPolicy.CLASS)
+}
